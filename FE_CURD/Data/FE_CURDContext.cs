dee9eb44faac220daed13820eb68775f7b1518fe
tuantamNTQ <tuan.vu2@ntq-solution.com.vn>
2022-12-01 08:54:31 +0700

    add project

---
+++ FE_CURD/Data/FE_CURDContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FE_CURD.Models;
+
+namespace FE_CURD.Data
+{
+    public class FE_CURDContext : DbContext
+    {
+        public FE_CURDContext (DbContextOptions<FE_CURDContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FE_CURD.Models.Movie> Movie { get; set; } = default!;
+    }
+}
